1f2a8676631ff4508a6437c866cfe71bd091db2f
Mohammad Saleem Wani <saleem@signitysolutions.com>
2025-01-24 10:16:51 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,16 @@
+const heading=React.createElement(
+    "h1",
+    {id:"heading",},
+    "Hello  World in React"
+);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+console.log(heading);
+
+
+{/* <div id="parent">
+    <div id="children">
+        <h1>Hiii This is headings</h1>
+    </div>
+</div> */}
